8ee8619e3fa7b290bf448605fdedb1403fb6f041
RojasJhonatan <jmr.85@hotmail.com>
2026-06-06 19:48:27 -0500

    base de datos, tablas

--- db/base.sql
+++ db/base.sql
@@ -8,5 +8,27 @@ CREATE TABLE eventos (
     fecha_hora Datetime NOT NULL,
     lugar VARCHAR(100) NOT NULL,
     capacidad_max int(10) NOT NULL,
-    precio_base
+    precio_base INT(10) NOT NULL
 );
+
+CREATE TABLE usuarios(
+     id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    rol VARCHAR(20)
+)
+
+CREATE TABLE tickets(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    evento_id INT NOT NULL,
+    usuario_id INT NOT NULL,
+    codigo INT NOT NULL UNIQUE,
+    fecha_compra TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    estado VARCHAR(20) DEFAULT "pagado",
+    FOREIGN KEY (evento_id) REFERENCES eventos(id)
+        ON DELETE RESTRICT ON UPDATE CASCADE,
+
+
+
+)
